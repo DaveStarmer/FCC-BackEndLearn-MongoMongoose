5169d17c0669b7d65f4d4730f31bbeee4f4c2fe3
Dave Starmer <dave@starmer.org.uk>
2022-03-24 21:54:19 +0000

    Database Schema

--- myApp.js
+++ myApp.js
@@ -1,10 +1,23 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-let Person;
+const personSchema = new mongoose.Schema ({
+  name: {type: String, required: true},
+  age: Number,
+  favouriteFoods: [String]
+});
+
+let Person = mongoose.model('Person',personSchema);
+
+
+
 
 const createAndSavePerson = (done) => {
+  let p = new Person();
   done(null /*, data*/);
 };
 
