f341cbc5ea9341c74262b1fd8613517a1e854e47
Dave Starmer <dave@starmer.org.uk>
2022-03-27 09:53:50 +0100

    changed to american spelling

--- myApp.js
+++ myApp.js
@@ -8,7 +8,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 const personSchema = new mongoose.Schema ({
   name: {type: String, required: true},
   age: Number,
-  favouriteFoods: [String]
+  favoriteFoods: [String]
 });
 
 let Person = mongoose.model('Person',personSchema);
